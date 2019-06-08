72aa7535e7817ae7ad8afb13f17d9f43e2cc95df
PrzemyslawMadej <przemyslawmd@gmail.com>
2019-06-08 17:33:35 +0200

    Tests added for creating Tree BST

--- Tests/TestTreeGeneral.cs
+++ Tests/TestTreeGeneral.cs
@@ -9,22 +9,48 @@ namespace Tests
     public class TestTreeGeneral
     {
         [TestMethod]
-        public void TestCreateBSTTree()
+        public void TestCreateBSTTree_1()
         {
-            List< int > keys = new List< int > { 3, 2, 1, 6 };
-            Tree tree = new TreeBST();
-            tree.CreateNodes( keys );
-
-            Stack< int > expectedNodes = new Stack< int >(); 
-            expectedNodes.Push( 3 );
-            expectedNodes.Push( 6 );
-            expectedNodes.Push( 2 );
-            expectedNodes.Push( 1 );
-
-            CheckNode( tree.Root, expectedNodes );
-            
+            List< int > keysToBuild = new List< int > { 3, 2, 1, 6 };
+            List< int > keysToCheck = new List< int > { 3, 6, 2, 1 };
+            CreateAndCheckTree( keysToBuild, keysToCheck );
         }
 
+        
+        [TestMethod]
+        public void TestCreateBSTTree_2()
+        {
+            List< int > keysToBuild = new List< int > { 13, 9, 2, 21, 32, 7, 3, 12, 8, 16 };
+            List< int > keysToCheck = new List< int > { 13, 21, 32, 16, 9, 12, 2, 7, 8, 3 };
+            CreateAndCheckTree( keysToBuild, keysToCheck );
+        }
+
+
+        [TestMethod]
+        public void TestCreateBSTTree_3()
+        {
+            List< int > keysToBuild = new List< int > 
+            { 
+                21, 12, 9, 32, 8, 36, 33, 17, 23, 34, 4, 19, 25, 6, 37, 3, 38, 27, 29, 24 
+            };
+            List< int > keysToCheck = new List< int > 
+            {  
+                21, 32, 36, 37, 38, 33, 34, 23, 25, 27, 29, 24, 12, 17, 19, 9, 8, 4, 6, 3 
+            };
+            CreateAndCheckTree( keysToBuild, keysToCheck );
+        }
+        
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        private void CreateAndCheckTree( List< int > keysToBuild, List< int > keysToCheck )
+        {
+            Tree tree = new TreeBST();
+            tree.CreateNodes( keysToBuild );
+            Stack< int > expected = new Stack< int >( keysToCheck ); 
+            CheckNode( tree.Root, expected );
+        }
+        
         /*******************************************************************************************/
         /*******************************************************************************************/
 
