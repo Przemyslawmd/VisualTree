e8fd910e444a5f26b76e9d81370a97a851f4d882
PrzemyslawMadej <przemyslawmd@gmail.com>
2019-06-14 23:39:40 +0200

    Control service added

---
+++ Source/ServiceControls.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Windows.Controls;
+
+namespace VisualTree
+{
+    class ServiceControls
+    {
+        public static ServiceControls GetInstance()
+        {
+            return serviceControls;
+        }
+        
+        public static void CreateServiceControls( Canvas canvas )
+        {
+            serviceControls = new ServiceControls( canvas );
+        }
+
+        public ServiceControls( Canvas canvas )
+        { 
+            Canvas = canvas;    
+        }
+        
+        public static ServiceControls serviceControls;
+        
+        public Canvas Canvas { get; }
+    }
+}
+
