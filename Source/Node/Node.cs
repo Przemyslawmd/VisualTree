f23fc0e61727a9db4f58ebe263b7dd5aff8006cc
PrzemyslawMadej <przemyslawmd@gmail.com>
2019-06-14 21:21:44 +0200

    Unnecessary value assignment removed

--- Source/Node/Node.cs
+++ Source/Node/Node.cs
@@ -6,22 +6,19 @@ namespace VisualTree
         public Node( int key )
         {
             Key = key;
-            Left = null;
-            Right = null;
-            Parent = null;
         }
         
         public int Key { get; }
         
-        public Node Parent {  get; set; } 
-        public Node Left {  get; set; } 
-        public Node Right {  get; set; } 
+        public Node Parent { get; set; } 
+        public Node Left   { get; set; } 
+        public Node Right  { get; set; } 
 
-        public int PosHor{ get; set; }
-        public int PosVer{ get; set; }
+        public int PosHor { get; set; }
+        public int PosVer { get; set; }
 
-        public int MatrixRow{ get; set; }
-        public int MatrixCol{ get; set; }
+        public int MatrixRow { get; set; }
+        public int MatrixCol { get; set; }
 
         public bool IsSelected { get; set; }
 
