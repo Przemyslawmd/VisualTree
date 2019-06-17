6b73244575ab51651cab43193e6fd549da4bc4e0
PrzemyslawMadej <przemyslawmd@gmail.com>
2019-06-18 00:20:33 +0200

    Working on AVL tree

--- Source/Node/NodeAVL.cs
+++ Source/Node/NodeAVL.cs
@@ -3,8 +3,26 @@ namespace VisualTree
 {
     class NodeAVL : Node
     {
-        NodeAVL( int key ) : base( key ) { }
+        public NodeAVL( int key ) : base( key ) { }
 
+        public new NodeAVL Parent { get; set; } 
+        public new NodeAVL Left   { get; set; } 
+        public new NodeAVL Right  { get; set; } 
+        
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        public bool VerifyAVL()
+        {
+            int leftLevel = IsLeft() ? Left.Level : 0;
+            int rightLevel = IsRight() ? Right.Level : 0;
+
+            return System.Math.Abs( leftLevel - rightLevel ) <= 1;
+        }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+         
         public int Level {  get; set; } = 1;
     }
 }
--- Source/Tree/TreeAVL.cs
+++ Source/Tree/TreeAVL.cs
@@ -5,8 +5,33 @@ namespace VisualTree
     {
         override public void AddNode( int key )
         {
-
+            NodeAVL node = new NodeAVL( key );
+            InsertNode( node );
+            CheckAVLPropertyAfterInsertNode( node );
         }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        private void CheckAVLPropertyAfterInsertNode( NodeAVL node )
+        {
+            if ( node.IsParent() is false )
+            {
+                return;
+            }
+
+            NodeAVL child = node;
+            node = node.Parent;
+        }
+            
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+        
+        private void FixAVLTree( NodeAVL node )
+        {
+            return;
+        }
+
     }
 }
 
