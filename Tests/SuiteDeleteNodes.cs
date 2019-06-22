079663a9e86bf0a54a964c6b092e6b5ccbae180d
PrzemyslawMadej <przemyslawmd@gmail.com>
2019-06-22 23:41:48 +0200

    Suite with tests for deleting AVL nodes added

--- Tests/SuiteDeleteNodes.cs
+++ Tests/SuiteDeleteNodes.cs
@@ -20,7 +20,7 @@ namespace Tests
 
         /*******************************************************************************************/
         /*******************************************************************************************/
-
+                
         // Node to be deleted has one child
         [TestMethod]
         public void TestDeleteNodesBST_2()
@@ -49,13 +49,75 @@ namespace Tests
         
         // Nodes to be deleted have both children and one node is root
         [TestMethod]
-        public void TestDeleteNodes_4()
+        public void TestDeleteNodesBST_4()
         {
             List< int > keysToBuild = new List< int > { 16, 8, 2, 11, 20, 25, 18, 1, 13, 3, 22, 21, 24, 10, 9, 17, 19 };
             List< int > keysToCheck = new List< int > { 1, 3, 2, 9, 10, 13, 11, 8, 19, 18, 24, 22, 25, 21, 17 };
             List< int > keysToDelete = new List< int > { 20, 16 };
             new Test().DeleteNodes( keysToBuild, keysToCheck, keysToDelete, TreeType.CommonBST );
         }
+        
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        // Nodes to be deleted have no children
+        [TestMethod]
+        public void TestDeleteNodesAVL_1()
+        {
+            List< int > keysToBuild = new List< int > { 6, 2, 5, 10, 1, 13, 4, 9 };
+            List< int > keysToCheck = new List< int > { 1, 2, 6, 13, 10, 5 };
+            List< int > keysToDelete = new List< int > { 4, 9 };
+            new Test().DeleteNodes( keysToBuild, keysToCheck, keysToDelete, TreeType.AVL );
+        }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        // Node to be deleted has one child
+        [TestMethod]
+        public void TestDeleteNodesAVL_2()
+        {
+            List< int > keysToBuild = new List< int > { 10, 5, 15, 3, 12, 1, 16, 2, 11 };
+            List< int > keysToCheck = new List< int > { 2, 1, 5, 3, 11, 16, 15, 10 };
+            List< int > keysToDelete = new List< int > { 12 };
+            new Test().DeleteNodes( keysToBuild, keysToCheck, keysToDelete, TreeType.AVL );
+        }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        // Nodes to be deleted have two children
+        [TestMethod]
+        public void TestDeleteNodesAVL_3()
+        {
+            List< int > keysToBuild = new List< int > { 10, 5, 6, 12, 11, 8, 2, 13, 9, 7 };
+            List< int > keysToCheck = new List< int > { 2, 5, 7, 9, 6, 11, 13, 10 };
+            List< int > keysToDelete = new List< int > { 12, 8 };
+            new Test().DeleteNodes( keysToBuild, keysToCheck, keysToDelete, TreeType.AVL );
+        }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        // Nodes to be deleted have both children and one node is root
+        [TestMethod]
+        public void TestDeleteNodesAVL_4()
+        {
+            List< int > keysToBuild = new List< int > { 16, 8, 2, 11, 20, 25, 18, 1, 13, 3, 22, 21, 24, 10, 9, 17, 19 };
+            List< int > keysToCheck = new List< int > { 1, 3, 2, 9, 10, 13, 11, 8, 18, 21, 19, 24, 25, 22, 17 };
+            List< int > keysToDelete = new List< int > { 20, 16 };
+            new Test().DeleteNodes( keysToBuild, keysToCheck, keysToDelete, TreeType.AVL );
+        }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Selection.DestroyInstance();
+        }
     }
 }
+
 
