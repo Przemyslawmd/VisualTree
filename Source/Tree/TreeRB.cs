6eafb83ef18d197f5574c7f09e2e6c9e1fe0bb15
PrzemyslawMadej <przemyslawmd@gmail.com>
2019-11-08 01:49:24 +0100

    An empty class for RB tree

---
+++ Source/Tree/TreeRB.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+
+namespace VisualTree
+{
+    class TreeRB : Tree
+    {
+        override public void AddNode( int key )
+        {
+            Node node = new Node( key );
+            InsertNode( node );
+        }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        override public void DelSelectedNodes( List< Node > nodes )
+        {
+            foreach ( Node node in nodes )
+            {
+                DetachNode( node );
+            }
+        }
+    }
+}
+
