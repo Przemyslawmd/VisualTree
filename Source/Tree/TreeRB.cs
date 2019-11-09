f8ae19222afbdcaeef790d0139912a0d766e2505
PrzemyslawMadej <przemyslawmd@gmail.com>
2019-11-09 14:23:25 +0100

    Basics examples for creating RB tree works properly

--- Source/Tree/TreeRB.cs
+++ Source/Tree/TreeRB.cs
@@ -13,7 +13,11 @@ namespace VisualTree
             if ( node.IsParent() is false )
             {
                 node.Color = NodeColor.BLACK;
+                return;
             }
+
+            CheckTreeRB( node );
+            RestoreRoot();
         }
 
         /*******************************************************************************************/
@@ -24,6 +28,80 @@ namespace VisualTree
             foreach ( Node node in nodes )
             {
                 DetachNode( node );
+            }
+        }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        public void CheckTreeRB( Node node )
+        {
+            while ( true )
+            {
+                if ( node is null || node.Parent is null || node.Parent.Color == NodeColor.BLACK )
+                {
+                    return;
+                }
+                
+                Node parent = node.Parent;
+                Node uncle = GetUncle( node );
+
+                if ( uncle != null && uncle.Color == NodeColor.RED )
+                {
+                    node = FixTreeByChangingColor( parent, uncle );
+                }
+                else
+                {
+                    node = FixTreeByRotation( node );
+                }
+            }
+        }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        private Node GetUncle( Node node )
+        {
+            Node parent = node.Parent;
+            Node grand = parent.Parent;
+
+            if ( grand is null )
+            {
+                return null;
+            }
+
+            return grand > parent ? grand.Right : grand.Left;
+        }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        private Node FixTreeByChangingColor( Node siblingA, Node siblingB )
+        {
+            siblingA.Color = NodeColor.BLACK;
+            siblingB.Color = NodeColor.BLACK;
+            siblingA.Parent.Color = siblingA.Parent.IsParent() ? NodeColor.RED : NodeColor.BLACK;
+            return siblingA.Parent;
+        }
+
+        /*******************************************************************************************/
+        /*******************************************************************************************/
+
+        private Node FixTreeByRotation( Node node )
+        {
+            Node parent = node.Parent;
+            Node grand = parent.Parent;
+
+            if (( grand > parent && parent > node ) || ( grand < parent && parent < node ))
+            {
+                parent.Color = NodeColor.BLACK;
+                grand.Color = NodeColor.RED;
+                return RotateNode( parent );
+            }
+            else 
+            {
+                node = RotateNode( node );
+                return RotateNode( node );
             }
         }
     }
